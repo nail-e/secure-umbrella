a1b62cbf0ef755eaede11fec3dc14573a3993026
nail-e <elianinigorieza@gmail.com>
2022-02-05 00:32:58 +0400

    Finished main.py structure, just needs functions from modules

--- main/main.py
+++ main/main.py
@@ -1,3 +1,33 @@
-from variables import *
+from unittest import skip
+from variables import *         #Imports all variables from variables.py
+import time
+from tqdm import tqdm           #Imports tqdm, a loading bar
 
-print(SecureUmbrella_title)
+#Local Variables 
+IsMenuDone = 0 
+MainMenuInput = 0
+
+SecureUmbrella_title()
+skipline()
+for i in tqdm (range(int(5e6)), desc="Initializing"):       #tqdm loading bar that lasts a second (5 million ticks)
+    pass
+while IsMenuDone == 0:
+    mainmenu()
+    skipline()
+    MainMenuInput = int(input("Choose an option: ")) 
+    if MainMenuInput == 1:
+        #function for Creating Password
+        IsMenuDone = IsMenuDone + 1
+    elif MainMenuInput == 2:
+        #function for Store Existing Password
+        IsMenuDone = IsMenuDone + 1
+    elif MainMenuInput == 3:
+        #function for S
+        IsMenuDone = IsMenuDone + 1
+    elif MainMenuInput == 4:
+        print("Exiting Program")
+        IsMenuDone = IsMenuDone + 1
+    else:
+        skipline()
+        print("Invalid Input!")
+
