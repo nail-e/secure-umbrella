0487977a9737b02780c65723016ef6a5662e6a0b
nail-e <elianinigorieza@gmail.com>
2022-02-05 19:42:19 +0400

    Added createfile.py

---
+++ main/createfile.py
@@ -0,0 +1,7 @@
+from variables import *
+import os                       #Imports OS module
+
+#Local Variables
+CurrentDirectory = os.getcwd()      #Retrieves which current directory the program is running in
+
+NewDirectory()
